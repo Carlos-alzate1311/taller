7b9dea3d4f7bdf648e32ab0b454254d18fcc4983
carlos-2022 <carlos.alzateg131122@gmail.com>
2024-08-28 22:50:33 -0500

    creacion_proyecto_conexion_base_de_datos

---
+++ tallerv1/src/main/java/com/tallerv1/tallerv1/Tallerv1Application.java
@@ -0,0 +1,13 @@
+package com.tallerv1.tallerv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tallerv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tallerv1Application.class, args);
+	}
+
+}
---
+++ tallerv1/src/test/java/com/tallerv1/tallerv1/Tallerv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallerv1.tallerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tallerv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
